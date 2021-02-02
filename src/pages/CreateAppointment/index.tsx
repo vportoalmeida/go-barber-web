92c04b4b2d8615b3ceed3062e5f12764e1cef30d
Vinicius Almeida <v.almeida1284@gmail.com>
2021-02-01 20:01:36 -0400

    Release 3.1.5 Check Readme for details

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -23,18 +23,6 @@ import {
   Provider,
   Select,
   Background,
-<<<<<<< HEAD
-  AgendaSelect,
-  Calendar,
-  Schedule,
-  Section,
-  Title,
-  SectionTitle,
-  SectionContent,
-  Hour,
-  HourText,
-=======
->>>>>>> 05b4f19acc9154eecee61bcbc29dc7277985267e
 } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
